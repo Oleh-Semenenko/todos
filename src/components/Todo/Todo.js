2c4d7d88123441355db3673b5140c7c8922bb779
Oleh Semenenko <o.semenenko.mar@gmail.com>
2023-03-02 11:47:57 +0200

    fixed checkbox click

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,28 +1,42 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Modal } from 'components/Modal/Modal';
 import './Todo.css';
 
 export const Todo = ({ id, title, description, status }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isCheckedTodo, setIsCheckedTodo] = useState(false);
+  const [isInputClick, setIsInputClick] = useState(false);
+
+  const checkboxRef = useRef();
 
   const changeStatus = () => {
     setIsCheckedTodo(!isCheckedTodo);
   };
 
+  const onCheckboxClick = () => {
+    if (checkboxRef.current) {
+      setIsInputClick(true);
+    }
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    setIsInputClick(false);
+  };
+
   return (
-    <tr onClick={() => setIsModalOpen(true)}>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{description}</td>
-      <td>
+    <tr>
+      <td onClick={openModal}>{id}</td>
+      <td onClick={openModal}>{title}</td>
+      <td onClick={openModal}>{description}</td>
+      <td className="checkbox" ref={checkboxRef} onClick={onCheckboxClick}>
         <input
           type="checkbox"
           checked={isCheckedTodo}
           onChange={changeStatus}
         />
       </td>
-      {isModalOpen && (
+      {isModalOpen && !isInputClick && (
         <Modal
           changeStatus={changeStatus}
           isCheckedTodo={isCheckedTodo}
